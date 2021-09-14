a0f837a29a229add97ccee602dd16c9967554ccb
Daniel Muñoz <djmv225@gmail.com>
2021-09-14 14:28:36 -0500

    Add the show log file view and controller

---
+++ app/controllers/log_files_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class LogFilesController < ApplicationController
+  before_action :set_log_file
+
+  def show
+    @file_url = ActiveStorage::Blob.service.path_for(@log_file.csv.key)
+    @pagy, @contacts = pagy(@log_file.contacts.order(:created_at), items: 20)
+    @contacts_error = @log_file.upload_errors.count > 0
+  end
+
+  private
+
+  def set_log_file
+    @log_file = LogFile.find(params[:id])
+  end
+end
